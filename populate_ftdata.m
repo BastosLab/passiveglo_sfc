788ebb170fb4f97b439014ad12a22569966b0a6f
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-08-07 17:48:23 -0500

    populate_ftdata: label channels as cell array of character lists
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- populate_ftdata.m
+++ populate_ftdata.m
@@ -2,9 +2,9 @@ function [data] = populate_ftdata(area, signals, channel_nums, timestamps)
 assert(size(signals, 1) == size(channel_nums, 2));
 
 data = [];
-data.label = {};
+data.label = cell(size(channel_nums, 2), 1);
 for c=1:size(channel_nums, 2)
-    data.label{c} = strcat(area, "_", string(channel_nums(c)));
+    data.label{c} = [area, '_', int2str(channel_nums(c))];
 end
 data.time = {};
 data.trial = {};
