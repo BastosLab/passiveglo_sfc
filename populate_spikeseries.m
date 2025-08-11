952dde60a92c1aed4a6feb7ef165bc6151d63e39
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-08-11 10:55:10 -0500

    populate_spikeseries: speed up with desearchn()
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- populate_spikeseries.m
+++ populate_spikeseries.m
@@ -1,12 +1,7 @@
 function [spikeseries] = populate_spikeseries(spike_times, timestamps)
 spikeseries = zeros(size(timestamps));
-dt = mean(diff(timestamps));
-for s=1:size(spike_times, 1)
-    spiketime_indices = find(abs(timestamps - spike_times(s)) <= dt);
-    for i=1:size(spiketime_indices, 1)
-        t = spiketime_indices(i);
-        spikeseries(t) = 1;
-    end
-end
+
+spike_samples = dsearchn(timestamps, spike_times);
+spikeseries(spike_samples) = 1;
 end
 
