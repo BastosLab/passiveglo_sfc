3a2af1572ef6ec7f14cc04d0042b971a786f341a
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-08-12 12:52:38 -0500

    populate_spikeseries: calculate continuous samples by round(spike-time / dt)
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- populate_spikeseries.m
+++ populate_spikeseries.m
@@ -1,7 +1,8 @@
 function [spikeseries] = populate_spikeseries(spike_times, timestamps)
 spikeseries = zeros(size(timestamps));
+dt = mean(diff(timestamps));
 
-spike_samples = dsearchn(timestamps, spike_times);
+spike_samples = round(spike_times / dt);
 spikeseries(spike_samples) = 1;
 end
 
