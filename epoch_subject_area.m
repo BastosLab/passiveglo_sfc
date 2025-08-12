f6c2a80370b8469f70adfe4b32c331cfcc2edf59
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-08-11 20:59:19 -0500

    epoch_subject_area: pass through mask for unit selection
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- epoch_subject_area.m
+++ epoch_subject_area.m
@@ -1,11 +1,15 @@
-function [session] = epoch_subject_area(session_dir, area, interval_tag, prestim, poststim)
+function [session] = epoch_subject_area(session_dir, area, interval_tag, prestim, poststim, units_mask)
 passive_glo = load([session_dir, '/', 'passiveglo_task_data.mat']);
 interval_starts = passive_glo.start_time(passive_glo.(interval_tag)) - prestim;
 interval_stops = passive_glo.stop_time(passive_glo.(interval_tag)) + poststim;
 
 session = epoch_subject_lfps(session_dir, area, interval_tag, ...
     interval_starts, interval_stops);
-session.(area).units = populate_area_units(session_dir, session.(area).channels);
+if exist('units_mask', 'var')
+    session.(area).units = populate_area_units(session_dir, session.(area).channels, units_mask);
+else
+    session.(area).units = populate_area_units(session_dir, session.(area).channels);
+end
 session.(area).units.unit_spikeseries = epoch_spikeseries(session.(area).units, ...
     session.timestamps, interval_starts, interval_stops);
 end
