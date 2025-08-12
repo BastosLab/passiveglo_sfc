cd9b2ad1c2bd706b1b3c6a2d4b34b3ef5289097a
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-08-12 12:30:49 -0500

    epoch_session_area: refactor to avoid duplicating information and computation
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- epoch_session_area.m
+++ epoch_session_area.m
@@ -1,16 +1,23 @@
-function [session] = epoch_session_area(session_dir, area, interval_tag, prestim, poststim, units_mask)
-passive_glo = load([session_dir, '/', 'passiveglo_task_data.mat']);
-interval_starts = passive_glo.start_time(passive_glo.(interval_tag)) - prestim;
-interval_stops = passive_glo.stop_time(passive_glo.(interval_tag)) + poststim;
+function [session] = epoch_session_area(session_dir, area, interval_tags, prestim, poststim, units_mask)
+session = session_area_channels(session_dir, area);
 
-session = epoch_subject_lfps(session_dir, area, interval_tag, ...
-    interval_starts, interval_stops);
 if exist('units_mask', 'var')
     session.(area).units = populate_area_units(session_dir, session.(area).channels, units_mask);
 else
     session.(area).units = populate_area_units(session_dir, session.(area).channels);
 end
-session.(area).units.unit_spikeseries = epoch_spikeseries(session.(area).units, ...
-    session.timestamps, interval_starts, interval_stops);
-end
 
+passive_glo = load([session_dir, '/', 'passiveglo_task_data.mat']);
+
+for i=1:size(interval_tags, 1)
+    interval_tag = interval_tags{i};
+    interval_starts = passive_glo.start_time(passive_glo.(interval_tag)) - prestim;
+    interval_stops = passive_glo.stop_time(passive_glo.(interval_tag)) + poststim;
+
+    session.(area).(interval_tag) = epoch_subject_lfps(session.(area), ...
+        interval_starts, interval_stops);
+    session.(area).(interval_tag).unit_spikeseries = epoch_spikeseries( ...
+        session.(area).units, session.timestamps, interval_starts, ...
+        interval_stops);
+end
+end
