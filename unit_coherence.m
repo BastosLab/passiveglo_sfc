1189f2c87d16b6480067b318a38517d1907293e9
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-08-12 16:29:58 -0500

    unit_coherence: allow a trials mask/index input argument
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- unit_coherence.m
+++ unit_coherence.m
@@ -1,9 +1,12 @@
-function [freqs, sfc] = unit_coherence(area, unit, condition)
+function [freqs, sfc] = unit_coherence(area, unit, condition, trials)
+if ~exist('trials', 'var')
+    trials = true(size(area.(condition).lfp, 3));
+end
 unit_channel = find(area.channels.id == area.units.channels(unit));
 lfp_channel = max(unit_channel - 10, 1);
 
-timeseries = cat(1, area.(condition).lfp(lfp_channel, :, :), ...
-    area.(condition).unit_spikeseries(unit, :, :));
+timeseries = cat(1, area.(condition).lfp(lfp_channel, :, trials), ...
+    area.(condition).unit_spikeseries(unit, :, trials));
 ftdata = populate_ftdata(timeseries, [area.channels.id(lfp_channel); ...
     area.channels.id(unit_channel)], area.(condition).timestamps);
 con = coherence(ftdata);
