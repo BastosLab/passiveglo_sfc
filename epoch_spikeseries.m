176f4e7e3a8c0dd336b6a1a7e72cbb03d4a4d690
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-08-12 12:53:03 -0500

    epoch_spikeseries: turn off parfor to avoid hanging Matlab
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- epoch_spikeseries.m
+++ epoch_spikeseries.m
@@ -4,7 +4,7 @@ stop_samples = nearest_index(timestamps, stops);
 num_samples = round(mean(stop_samples - start_samples));
 spikeseries = zeros(size(area_units.id, 1), num_samples+1, size(starts, 1));
 
-parfor au=1:size(area_units.id, 1)
+for au=1:size(area_units.id, 1)
     unit_spike_times = area_units.unit_spike_times{au};
     fitting = unit_spike_times >= min(starts) & unit_spike_times <= max(stops);
     series = populate_spikeseries(unit_spike_times(fitting), timestamps);
