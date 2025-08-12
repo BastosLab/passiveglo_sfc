9fa73737ffb47df58a91787677d6bfac51f65319
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-08-11 21:00:36 -0500

    epoch_session_area: rename function
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- epoch_session_area.m
+++ epoch_session_area.m
@@ -1,4 +1,4 @@
-function [session] = epoch_subject_area(session_dir, area, interval_tag, prestim, poststim, units_mask)
+function [session] = epoch_session_area(session_dir, area, interval_tag, prestim, poststim, units_mask)
 passive_glo = load([session_dir, '/', 'passiveglo_task_data.mat']);
 interval_starts = passive_glo.start_time(passive_glo.(interval_tag)) - prestim;
 interval_stops = passive_glo.stop_time(passive_glo.(interval_tag)) + poststim;
