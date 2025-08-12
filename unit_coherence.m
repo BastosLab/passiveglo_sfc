362e931c5fa30df3bc7b56a71f9f4ce888ae3336
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-08-12 12:31:32 -0500

    unit_coherence: locate spikeseries under tagged condition
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- unit_coherence.m
+++ unit_coherence.m
@@ -3,7 +3,7 @@ unit_channel = find(area.channels.id == area.units.channels(unit));
 lfp_channel = max(unit_channel - 10, 1);
 
 timeseries = cat(1, area.(condition).lfp(lfp_channel, :, :), ...
-    area.units.unit_spikeseries(unit, :, :));
+    area.(condition).unit_spikeseries(unit, :, :));
 ftdata = populate_ftdata(timeseries, [area.channels.id(lfp_channel); ...
     area.channels.id(unit_channel)], area.(condition).timestamps);
 con = coherence(ftdata);
