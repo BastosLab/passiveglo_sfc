7c4fcb16e32f085490f7773b90e4ad3a6ec1559b
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-08-18 13:05:01 -0500

    area_sts: pass in FOI and find strongest channel in that band
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- area_sts.m
+++ area_sts.m
@@ -1,4 +1,4 @@
-function [sts] = area_sts(area, condition)
+function [sts] = area_sts(area, condition, foi)
 unit_channel_ids = unique(area.units.channels);
 unitfree_channels = true(size(area.channels.id));
 for c=1:size(unitfree_channels, 1)
@@ -13,11 +13,27 @@ ftlfp = populate_ftdata(lfp, lfp_channels, area.(condition).timestamps);
 ftspikeseries = spikeseries_ftdata(area.(condition).unit_spikeseries, ...
     area.units.id, area.(condition).timestamps);
 
+% Figure out the strongest channel
 cfg = [];
-cfg.foi = 10:90;
+cfg.method = 'mtmfft';
+cfg.output = 'pow';
+cfg.foi = foi;
+cfg.taper = 'hann';
+cfg.t_ftimwin = 9./cfg.foi;
+lfp_freqs = ft_freqanalysis(cfg, ftlfp);
+[~, strongest_channel] = max(mean(lfp_freqs.powspctrm, 2));
+
+ftlfp.label = {ftlfp.label{strongest_channel}};
+for t=1:size(ftlfp.trial, 2)
+    ftlfp.trial{:, t} = ftlfp.trial{:, t}(strongest_channel, :);
+end
+
+cfg = [];
+cfg.foi = foi;
 cfg.taper = 'hann';
 cfg.t_ftimwin = 9./cfg.foi; % 9 cycles per frequency as per the paper
 
 sts = ft_spiketriggeredspectrum_convol(cfg, ftlfp, ftspikeseries);
+sts.spike = ftspikeseries;
 end
 
