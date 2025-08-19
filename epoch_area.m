10641c2e9c4ef00c1fe53bb8a5800b08d96464d0
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-08-19 18:36:51 -0500

    epoch_area: function to epoch an entire area
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

---
+++ epoch_area.m
@@ -0,0 +1,13 @@
+function [sessions] = epoch_area(sessions_dir, area, interval_tags, prestim, poststim)
+sessions = [];
+session_dirs = dir([sessions_dir, '/sub-*']);
+for s=1:size(session_dirs, 1)
+    session_name = session_dirs(s).name;
+    fieldname = matlab.lang.makeValidName(session_name);
+    if isfolder([sessions_dir, '/', session_name])
+        sessions.(fieldname) = epoch_session_area([sessions_dir, '/', session_name], ...
+            area, interval_tags, prestim, poststim);
+    end
+end
+end
+
