f1b7f0c22ee47d417b002cb4cc38648161abffc0
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-08-19 15:14:06 -0500

    spikeseries_ftdata: use absolute unit-ids to index into accepted units
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- spikeseries_ftdata.m
+++ spikeseries_ftdata.m
@@ -1,10 +1,13 @@
-function [ftdata] = spikeseries_ftdata(spikeseries, unit_ids, timestamps)
-assert(size(spikeseries, 1) == size(unit_ids, 1));
+% function [ftdata] = spikeseries_ftdata(spikeseries, unit_ids, timestamps)
+function [ftdata] = spikeseries_ftdata(timeseries, unit_ids)
+
+assert(size(timeseries.unit_spikeseries, 1) == sum(timeseries.units_accepted, 1));
+unit_ids = unit_ids(timeseries.units_accepted);
 labels = cell(size(unit_ids, 1), 1);
 for u=1:size(unit_ids, 1)
     labels{u} = ['Unit ', int2str(unit_ids(u))];
 end
-ftdata = populate_ftdata(spikeseries, string(labels), timestamps);
+ftdata = populate_ftdata(timeseries.unit_spikeseries, string(labels), timeseries.timestamps);
 ftdata = ft_checkdata(ftdata, 'datatype', 'spike', 'feedback', 'yes');
 end
 
