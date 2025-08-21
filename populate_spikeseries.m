e558549973ce63af01490ec185937a1d4a8dabeb
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-08-21 18:14:46 -0500

    populate_spikeseries: subtract timestamps(1), locate samples within timestamps
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- populate_spikeseries.m
+++ populate_spikeseries.m
@@ -1,8 +1,11 @@
 function [spikeseries] = populate_spikeseries(spike_times, timestamps)
-spikeseries = zeros(size(timestamps));
+spikeseries = zeros(size(timestamps, 1), 1);
 dt = mean(diff(timestamps));
+within = spike_times >= timestamps(1) & spike_times <= timestamps(end);
+spike_times = spike_times(within);
 
-spike_samples = round(spike_times / dt);
-spikeseries(spike_samples) = 1;
+spike_samples = int64(floor((spike_times - timestamps(1)) / dt));
+spikeseries(spike_samples, :) = 1;
+assert(size(spikeseries, 1) == size(timestamps, 1));
 end
 
