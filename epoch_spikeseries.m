ebf7e3a13f0bab4d36c90e1224718a0dc1584725
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-08-21 18:17:09 -0500

    epoch_spikeseries: pass in raw spikeseries, not spike-times
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- epoch_spikeseries.m
+++ epoch_spikeseries.m
@@ -1,14 +1,7 @@
-function [spikeseries] = epoch_spikeseries(area_units, timestamps, starts, stops)
+function [epoched] = epoch_spikeseries(spikeseries, timestamps, starts, stops)
 start_samples = nearest_index(timestamps, starts);
 stop_samples = nearest_index(timestamps, stops);
 num_samples = round(mean(stop_samples - start_samples));
-spikeseries = zeros(size(area_units.id, 1), num_samples+1, size(starts, 1));
-
-for au=1:size(area_units.id, 1)
-    unit_spike_times = area_units.unit_spike_times{au};
-    fitting = unit_spike_times >= min(starts) & unit_spike_times <= max(stops);
-    series = populate_spikeseries(unit_spike_times(fitting), timestamps);
-    spikeseries(au, :, :) = epoch_data(series', start_samples, [0, num_samples]);
-end
+epoched = epoch_data(spikeseries, start_samples, [0, num_samples]);
 end
 
