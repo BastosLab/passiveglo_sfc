a0ca463bb59a3f190166220b86430b0a1dd4929c
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-08-21 18:17:35 -0500

    epoch_area: fold prestim, poststim buffer times into condition definition
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- epoch_area.m
+++ epoch_area.m
@@ -1,4 +1,4 @@
-function [sessions] = epoch_area(sessions_dir, area, interval_tags, prestim, poststim)
+function [sessions] = epoch_area(sessions_dir, area, interval_tags)
 sessions = [];
 session_dirs = dir([sessions_dir, '/sub-*']);
 for s=1:size(session_dirs, 1)
@@ -6,7 +6,7 @@ for s=1:size(session_dirs, 1)
     fieldname = matlab.lang.makeValidName(session_name);
     if isfolder([sessions_dir, '/', session_name])
         sessions.(fieldname) = epoch_session_area([sessions_dir, '/', session_name], ...
-            area, interval_tags, prestim, poststim);
+            area, interval_tags);
     end
 end
 end
