e071562dfb21b522a1ff182cc5cc1f4c75843ca6
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-08-21 18:13:43 -0500

    spikeseries_ftdata: pass in accepted units instead of computing them
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- spikeseries_ftdata.m
+++ spikeseries_ftdata.m
@@ -1,8 +1,6 @@
 % function [ftdata] = spikeseries_ftdata(spikeseries, unit_ids, timestamps)
 function [ftdata] = spikeseries_ftdata(timeseries, unit_ids)
 
-assert(size(timeseries.unit_spikeseries, 1) == sum(timeseries.units_accepted, 1));
-unit_ids = unit_ids(timeseries.units_accepted);
 labels = cell(size(unit_ids, 1), 1);
 for u=1:size(unit_ids, 1)
     labels{u} = ['Unit ', int2str(unit_ids(u))];
