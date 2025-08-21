45919a072da1abaaee6cbec449e5f5eff64379b5
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-08-21 18:16:37 -0500

    epoch_session_area: calculate accepted units across whole session's spikeseries
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- epoch_session_area.m
+++ epoch_session_area.m
@@ -1,37 +1,43 @@
-function [session] = epoch_session_area(session_dir, area, interval_tags, prestim, poststim, units_mask)
+function [session] = epoch_session_area(session_dir, area, interval_tags, units_mask)
 session = session_area_channels(session_dir, area);
-
+session_length = session.timestamps(end) - session.timestamps(1);
 if exist('units_mask', 'var')
     session.(area).units = populate_area_units(session_dir, session.(area).channels, units_mask);
 else
     session.(area).units = populate_area_units(session_dir, session.(area).channels);
 end
+
+session.(area).units.accepted = false(size(session.(area).units.id, 1), 1);
 session.(area).units.location = cell(size(session.(area).units.id, 1), 1);
+spikeseries = zeros(size(session.(area).units.id, 1), size(session.timestamps, 1));
 for u=1:size(session.(area).units.id, 1)
     channel = session.(area).units.channels(u);
     where = session.(area).channels.id == channel;
     session.(area).units.location{u} = session.(area).channels.location{where};
+    spikeseries(u, :) = populate_spikeseries(session.(area).units.unit_spike_times{u}, ...
+        session.timestamps)';
 end
+session.(area).units.accepted = (sum(spikeseries, 2) / session_length) > 1;
+
+accepted_units = [];
+accepted_units.id = session.(area).units.id(session.(area).units.accepted);
+accepted_units.channels = session.(area).units.channels(session.(area).units.accepted);
+accepted_units.unit_index = session.(area).units.unit_index(session.(area).units.accepted);
+accepted_units.unit_spike_times = session.(area).units.unit_spike_times(session.(area).units.accepted);
+accepted_units.location = session.(area).units.location(session.(area).units.accepted);
 
 passive_glo = load([session_dir, '/', 'passiveglo_task_data.mat']);
 
 for i=1:size(interval_tags, 1)
-    interval_tag = interval_tags{i};
-    interval_starts = passive_glo.start_time(interval_tag(passive_glo)) - prestim;
-    interval_stops = passive_glo.stop_time(interval_tag(passive_glo)) + poststim;
-    interval_lengths = mean(interval_stops - interval_starts);
+    [interval_tag, prestim, poststim] = interval_tags{i}(passive_glo);
+    interval_starts = passive_glo.start_time(interval_tag) - prestim;
+    interval_stops = passive_glo.stop_time(interval_tag) + poststim;
 
     session.(area).fsample = round(1 / mean(diff(session.timestamps)));
-    session.(area).(func2str(interval_tag)) = epoch_subject_lfps(session.(area), ...
+    session.(area).(func2str(interval_tags{i})) = epoch_subject_lfps(session.(area), ...
         interval_starts, interval_stops);
-    session.(area).(func2str(interval_tag)).unit_spikeseries = epoch_spikeseries( ...
-        session.(area).units, session.timestamps, interval_starts, ...
-        interval_stops);
-
-    spikes_per_second = mean(sum(session.(area).(func2str(interval_tag)).unit_spikeseries(:, :, :), ...
-        2), 3) / interval_lengths;
-    accept = spikes_per_second > 1;
-    session.(area).(func2str(interval_tag)).units_accepted = accept;
-    session.(area).(func2str(interval_tag)).unit_spikeseries = session.(area).(func2str(interval_tag)).unit_spikeseries(accept, :, :);
+    session.(area).(func2str(interval_tags{i})).unit_spikeseries = epoch_spikeseries( ...
+        spikeseries(session.(area).units.accepted, :), session.timestamps, ...
+        interval_starts, interval_stops);
 end
 end
