54931625bbefb3e455df618dd123217c919145e1
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-08-26 13:37:01 -0500

    locate_units: locate units across sessions
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

---
+++ locate_session_units.m
@@ -0,0 +1,8 @@
+function [unit_indices] = locate_session_units(area, conditions, locater)
+located_units = locater(area.units.location);
+unit_indices = [];
+for c=1:size(conditions, 1)
+    unit_indices.(conditions{c}) = intersect(find(located_units), area.(conditions{c}).accepted);
+end
+end
+
--- locate_units.m
+++ locate_units.m
@@ -1,8 +1,19 @@
-function [unit_indices] = locate_units(area, conditions, locater)
-located_units = locater(area.units.location);
+function [unit_indices] = locate_units(sessions, area, conditions, locater)
+names = fieldnames(sessions);
 unit_indices = [];
-for c=1:size(conditions, 1)
-    unit_indices.(conditions{c}) = intersect(find(located_units), area.(conditions{c}).accepted);
+for s=1:size(names, 1)
+    session_unit_indices = locate_session_units(sessions.(names{s}).(area), ...
+        conditions, locater);
+    for c=1:size(conditions, 1)
+        session = ones(size(session_unit_indices.(conditions{c}), 1), 1) * s;
+        indices = cat(2, session, session_unit_indices.(conditions{c}));
+        if s == 1
+            unit_indices.(conditions{c}) = indices;
+        else
+            unit_indices.(conditions{c}) = cat(1, unit_indices.(conditions{c}), ...
+                indices);
+        end
+    end
 end
 end
 
