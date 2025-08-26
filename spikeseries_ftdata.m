5156fd093af6f4a969976ccc01934ed9f74b9eb9
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-08-26 12:59:18 -0500

    spikeseries_ftdata: populate, label spikeseries FT data from session-condition
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- spikeseries_ftdata.m
+++ spikeseries_ftdata.m
@@ -1,11 +1,12 @@
-% function [ftdata] = spikeseries_ftdata(spikeseries, unit_ids, timestamps)
-function [ftdata] = spikeseries_ftdata(timeseries, unit_ids)
+function [ftdata] = spikeseries_ftdata(condition_data, unit_ids)
 
+unit_ids = unit_ids(condition_data.accepted);
 labels = cell(size(unit_ids, 1), 1);
 for u=1:size(unit_ids, 1)
     labels{u} = ['Unit ', int2str(unit_ids(u))];
 end
-ftdata = populate_ftdata(timeseries.unit_spikeseries, string(labels), timeseries.timestamps);
+ftdata = populate_ftdata(condition_data.unit_spikeseries, string(labels), ...
+    condition_data.timestamps);
 ftdata = ft_checkdata(ftdata, 'datatype', 'spike', 'feedback', 'yes');
 end
 
