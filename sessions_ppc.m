827e7e17c8495f0c30985bae0024cd6eec42e915
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-08-26 12:56:48 -0500

    sessions_ppc: include within-session unit indices in PPC results
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- sessions_ppc.m
+++ sessions_ppc.m
@@ -7,15 +7,20 @@ for c=1:size(conditions, 1)
         session = sessions.(session_names{s});
         sts = area_sts(session.(area), conditions{c}, foi);
         ppc = units_ppc(sts, sts.spike);
+        unit_indices = cat(2, ones(size(ppc.ppc1, 1), 1) * s, ...
+                session.(area).(conditions{c}).accepted);
         if s == 1
             ppcs.(conditions{c}).freqs = sts.freq';
             ppcs.(conditions{c}).ppc = ppc.ppc1;
             ppcs.(conditions{c}).num_spikes = ppc.num_spikes;
+            ppcs.(conditions{c}).unit_indices = unit_indices;
         else
             ppcs.(conditions{c}).ppc = cat(1, ppcs.(conditions{c}).ppc, ...
                 ppc.ppc1);
             ppcs.(conditions{c}).num_spikes = cat(1, ...
                 ppcs.(conditions{c}).num_spikes, ppc.num_spikes);
+            ppcs.(conditions{c}).unit_indices = cat(1, ...
+                ppcs.(conditions{c}).unit_indices, unit_indices);
         end
     end
 end
